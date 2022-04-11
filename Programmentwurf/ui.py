3f8eb9a7df5106c6bc53a62d6b6311a23a066157
Eliaskgit <e@keop.de>
2022-04-11 16:43:09 +0200

    pvp_or_pve und welcome test

--- Programmentwurf/ui.py
+++ Programmentwurf/ui.py
@@ -41,7 +41,7 @@ class UI:
         x = input(print("Wollen sie schon ausgewählte Würfek wieder in den Becher werfen?"
                         "Falls Nein geben sie 0 ein, ansosnen den Würfelindex"))
         jjjjjj
-
+kkkkkkk
 
     def choose_action_with_dice_arr(self, dict) -> int:
         """
