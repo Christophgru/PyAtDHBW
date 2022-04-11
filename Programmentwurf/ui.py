4bb50e92ba2036fc5afc036984f9170eec2ccabb
Eliaskgit <e@keop.de>
2022-04-11 20:36:26 +0200

    choosename, endgame, chooseplayer

--- Programmentwurf/ui.py
+++ Programmentwurf/ui.py
@@ -4,9 +4,8 @@ import spielblock
 
 
 class UI:
-    def __init__(self, sb:spielblock.Spielblock):
+    def __init__(self):
         self.output = None
-        self.spielblock=sb
 
     def choosename(self, playernumber) -> string:
         name = input(print("Spieler ", playernumber, ", bitte wählen sie ihren Namen"))
@@ -25,7 +24,7 @@ class UI:
             else:
                 print("Geben sie bitte nur 1 oder 2 ein")
 
-    def choosediceorcheck(self, wuerfel_im_becher: dict):
+    def choosediceorcheck(self, schon_gewaehlte_wuerfel_dict: dict, wuerfel_im_becher: dict):
         """
             todo: Elias: waehle aus gewuerfelte_wuerfel_dict aus den aktiven wuerfeln (dice.isactivated->bool) die
             passenden aus
@@ -38,7 +37,7 @@ class UI:
             print("Gewählte Würfel:\n")
             q = 1
             for wuerfel in wuerfel_im_becher.values():
-                if not wuerfel.isactivated:
+                if wuerfel.isdeactivated:
                     print(q, ":", wuerfel.augen)
                 q += 1
 
@@ -48,8 +47,8 @@ class UI:
                 if wuerfelx.isactivated:
                     print(q, ":", wuerfelx.augen)
                 q += 1
-            eing = input("Wollen sie schon ausgewählte Würfek wieder in den Becher werfen "
-                         "oder gewürfelte Würfel beiseite legen, dann geben si ebitte den Würfelindex ein "
+            eing = input("Wollen sie schon ausgewählte Würfek wieder in den Becher werfen\n"
+                         "oder gewürfelte Würfel beiseite legen, dann geben si ebitte den Würfelindex ein\n"
                          "Bei mehreren Würfeln den Würfelindex bitte ohne Leerzeichen eingeben und mit Komma trennen")
             eing = eing.split(",")
             for x in eing:
@@ -59,7 +58,7 @@ class UI:
                     else:
                         wuerfel_im_becher[int(x)-1].activate()
                 elif x != 0:
-                    print("Wählen sie bite bloß 1,2,3,4 oder 5 aus")
+                    print("Wählen sie bitte bloß 1,2,3,4 oder 5 aus")
                     again = True
 
 
@@ -68,7 +67,7 @@ class UI:
            todo: Elias:abfrage an user was mit den gewürfelten würfeln eingetragen werden soll. rückgabe int/string mit steve
            absprechen, je nachdem was er haben will um zeile zu identifizieren
                """
-        self.spielblock.ausgabe()
+        spielblock.ausgabe()
         while True:
             try:
                 x = input("Geben sie bitte die Zeile an in welche sie das Gewürfelte eintragen wollen")
@@ -86,10 +85,10 @@ class UI:
         print("Es ist ", playername, "dran")
 
     def endgame(self, winner):
-        self.spielblock.ausgabe()
+        spielblock.ausgabe()
         print("Der Gewinner ist", winner)
 
     def welcome(self):
         """todo: Elias: ausgabe: herzlich willkommen, spielregeln usw."""
-        print("Herzlich willkommen bei Kniffel, sie können Player vs Player oder Player vs Computer spielen."
+        print("Herzlich willkommen bei Kniffel, sie können Player vs Player oder Player vs Computer spielen.\n"
               "Falls sie die Spielregeln noch nicht kennen google sie sie bitte .")
